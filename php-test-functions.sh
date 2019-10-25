2d42d370a759c1870b7ea3646957f098a27a1f5e
Angelos Giannis <38913957+Angelos-Giannis@users.noreply.github.com>
2019-10-25 17:42:12 +0300

    Convert aliases for modified files to one function

--- php-test-functions.sh
+++ php-test-functions.sh
@@ -1,20 +1,79 @@
-# Run all the modified unit test at once.
-alias phpunit_modified="git status | grep 'Test.php' | grep -v 'deleted:' | sed s/modified:// | xargs -I % sh -c 'printf \"\e[93;4;255m\nRunning file : %\e[0m\n\"; php vendor/bin/phpunit %'"
+# Custom function
+# Execution : phpFilesModified [-f] [-u]
+# Example   :
+#    - Run for modified test files only
+#	     phpFilesModified -u
+#    - Run for modified files only
+#        phpFilesModified -f
+#    - Run for all modified files and unit test files
+#        phpFilesModified -f -u
+function phpFilesModified() {
+	usage="
+Script usage: $(basename $0) [-f] [-u]
+By default both searching for modified files and modified unit test files is disabled
+Explanation of allowed flags/arguments :
+  - Helping :
+    -h             : Displays the usage of the script (this description)
+  - Optional arguments :
+    -f  : Search for files that have been modified and find the respective test files (same name with 'Test' suffix)
+    -u  : Search for unit test files that have been modified
+"
 
-# Run all the tests files related to modified files at once
-alias phpfiles_modified="git status | grep '.php' | grep -v 'deleted:' | grep -v 'Test.php' | sed s/modified:// | sed 's/src/tests\/Test/' | sed 's/.php/Test.php/' | grep -v 'renamed:' | xargs -I % sh -c 'ls % 2>/dev/null' | xargs -I % sh -c 'printf \"\e[93;4;255m\nRunning file : %\e[0m\n\"; php vendor/bin/phpunit %'"
+	check_files=false
+	check_unit_tests=false
 
+	while getopts fhu OPTION; do
+		case "$OPTION" in
+			f)
+				check_files=true
+				;;
+			u)
+				check_unit_tests=true
+				;;
+			h)
+				echo $usage >&2
+				return
+				;;
+			?)
+				echo $usage >&2
+				return
+				;;
+			*)
+				echo "Invalid arguments provided!!" >&2
+				echo $usage >&2
+				return
+				;;
+		esac
+	done
+
+	declare -a files_affected
+	declare -a tests_affected
+
+	[[ $check_files == true ]] && files_affected=($(git status | grep '.php' | grep -v 'deleted:' | grep -v 'Test.php' | sed s/modified:// | sed 's/src/tests\/Test/' | sed 's/.php/Test.php/' | grep -v 'renamed:'))
+	[[ $check_unit_tests == true ]] && tests_affected=($(git status | grep 'Test.php' | grep -v 'deleted:' | sed s/modified://))
+
+	# Cleanup the test files to be executed in order to remove duplicates
+	combined=( "${tests_affected[@]}" "${files_affected[@]}" )
+	unique_test_files=($(echo "${combined[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+	if [ -z "$unique_test_files" ]; then
+		echo "No test files to run found!!"
+		return
+	fi
+
+	__runTests $unique_test_files
+}
+
+# Custom function
 # Execution : phpTestCase -t <name of test> [-n <name of file where the test exists>] [-c|-f]
 # Example   : 
 #    - Run any occurance the test case  :
-#        phpTestCase -t testCase -c
+#        phpTestCase -t testGetMessageOnDriverComment -c
 #    - Run only the test case from file :
-#        phpTestCase -t testCase -f OneServiceTest.php -c
+#        phpTestCase -t testGetMessageOnDriverComment -f MessageServiceTest.php -c
 #    - Run the whole file of any occurance of the test case : 
-#        phpTestCase -t testCase -f
+#        phpTestCase -t testGetMessageOnDriverComment -f
 function phpTestCase() {
-	cmd="php vendor/bin/phpunit"
-
 	usage="
 Script usage: $(basename $0) -t test_case_name [-c | -f] [-n file_name] [-e]
 Explanation of allowed flags/arguments :
@@ -65,7 +124,7 @@ Explanation of allowed flags/arguments :
 				return
 				;;
 			*)
-				echo "Invalid argument provided!!" >&2
+				echo "Invalid arguments provided!!" >&2
 				echo $usage >&2
 				return
 				;;
@@ -102,7 +161,17 @@ Explanation of allowed flags/arguments :
 # Custom function
 # Execute all the test files that have been modified themselves or the respective class has been modified
 # as part of a branch as compared to develop branch.
-# Example : phpTestBranchChanges [base_branch]
+# Example :
+#    - Run all the modified test files and respective test files of modified files (compared to local develop)
+#        phpTestBranchChanges
+#    - Run modified files of branch compared to another branch
+#        phpTestBranchChanges -d <destination branch>
+#    - Run modified files of provided branch compared to default branch (develop)
+#        phpTestBranchChanges -b <feature branch>
+#    - Run modified files compared to remote branch (develop by default)
+#        phpTestBranchChanges -r
+#    - Run modified files of branch compared to another branch on remote
+#        phpTestBranchChanges -b <feature branch> -d <destination branch> -r
 function phpTestBranchChanges() {
 	cmd="php vendor/bin/phpunit"
 
@@ -142,7 +211,7 @@ Explanation of allowed flags/arguments :
 				return
 				;;
 			*)
-				echo "Invalid argument provided!!" >&2
+				echo "Invalid arguments provided!!" >&2
 				echo $usage >&2
 				return
 				;;
@@ -162,7 +231,16 @@ Explanation of allowed flags/arguments :
 	combined=( "${tests_affected[@]}" "${files_affected[@]}" )
 	unique_test_files=($(echo "${combined[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-	for f in "${unique_test_files[@]}"
+	__runTests $unique_test_files
+}
+
+# Run all the test files on the array provided
+function __runTests() {
+	test_files=("$@")
+
+	cmd="php vendor/bin/phpunit"
+
+	for f in "${test_files[@]}"
 	do
 		if [ -f "$f" ]; then
 			printf "\e[93;4;255m\nRunning file : ${f}\e[0m\n\n"
